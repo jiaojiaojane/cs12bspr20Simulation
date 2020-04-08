8e9e9e5ec6032af4cb36f45ac852eea58caacf56
FeipengQi <qfpeng99@brandeis.com>
2020-04-07 23:29:50 -0400

    move every 7 days

--- src/lesson19/MoveEvery7Days.java
+++ src/lesson19/MoveEvery7Days.java
@@ -26,30 +26,11 @@ public class MoveEvery7Days extends Person {
 	 * 
 	 */
 	public void tryToMove() {
-		tryToMoveRandomly();
-	}
-	
-    public void tryToMoveRandomly(){
-    	int dx = r.nextInt(3)-1; // -1,0,1
-		int dy = r.nextInt(3)-1; // -1,0,1
-		if (isOK(this.x+dx, this.y+dy,this.country)) {
-			this.moveTo(this.x+dx, this.y+dy);
+		if(this.counter%7==0) {
+			tryToMoveRandomly();
 			this.counter++;
 		}
-    }
-    
-    public boolean isOK(int a, int b,Country country) {
-		if (a<0 || a>=country.places.length || b<0
-				|| b>= country.places[0].length) {
-			return false;
-		}else if (country.places[a][b] !=null) {
-			return false;
-		}else if(this.counter%7!=0) {
-			return false;
-		}else {
-			return true;
-		}
-
 	}
-
+	
+    
 }
