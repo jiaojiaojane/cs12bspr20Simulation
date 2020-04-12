6870cfd002bf65fb695ba1e14065a2556dca6fff
wang2021 <62254195+wang2021@users.noreply.github.com>
2020-04-12 11:45:47 +0800

    Update Skeptic.java

--- src/lesson19/Skeptic.java
+++ src/lesson19/Skeptic.java
@@ -4,4 +4,9 @@ public class Skeptic extends Person {
     public Skeptic(int x, int y, Country country) {
         super(x, y, country);
     }
+
+    @Override
+    public void tryToMove() {
+
+    }
 }
