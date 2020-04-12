8fb8ffb1c57ca235445778977e3a4b11017982a3
wang2021 <62254195+wang2021@users.noreply.github.com>
2020-04-12 12:08:26 +0800

    Update Skeptic.java
    try to move randomly

--- src/lesson19/Skeptic.java
+++ src/lesson19/Skeptic.java
@@ -1,12 +1,15 @@
 package lesson19;
 
+
 public class Skeptic extends Person {
+
     public Skeptic(int x, int y, Country country) {
         super(x, y, country);
     }
 
     @Override
     public void tryToMove() {
-
+        super.tryToMoveRandomly();
     }
 }
+
