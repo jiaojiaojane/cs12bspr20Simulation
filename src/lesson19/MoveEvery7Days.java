7ee89d653c2ef8207fbd38d8309bf345cfd4067c
FeipengQi <qfpeng99@brandeis.com>
2020-04-13 22:25:51 -0400

    description of move every 7 days

--- src/lesson19/MoveEvery7Days.java
+++ src/lesson19/MoveEvery7Days.java
@@ -1,27 +1,22 @@
 /**
- * 
+ *
  */
 package lesson19;
 
 /**
  * @author Feipeng Qi
- *
+ *this class extends the skeptic class and creates a new type of person who moves every 7 days.
  */
 public class MoveEvery7Days extends Skeptic {
 	private int counter=0;
 
-	/**
-	 * @param x
-	 * @param y
-	 * @param country
-	 */
 	public MoveEvery7Days(int x, int y, Country country) {
 		super(x, y, country);
 		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * 
+	 * add a restriction to the tryToMove method so that the person moves every 7 days. 
 	 */
 	public void tryToMove() {
 		if(this.counter%7==0) {
@@ -29,6 +24,6 @@ public class MoveEvery7Days extends Skeptic {
 		}
 		this.counter++;
 	}
-	
-    
+
+
 }
