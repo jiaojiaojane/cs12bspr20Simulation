cf789ed2c0d79e50fd8696a259bb59907bc245ad
FeipengQi <qfpeng99@brandeis.com>
2020-04-13 22:32:19 -0400

    moveevery7days extend person

--- src/lesson19/MoveEvery7Days.java
+++ src/lesson19/MoveEvery7Days.java
@@ -7,7 +7,7 @@ package lesson19;
  * @author Feipeng Qi
  *this class extends the skeptic class and creates a new type of person who moves every 7 days.
  */
-public class MoveEvery7Days extends Skeptic {
+public class MoveEvery7Days extends Person {
 	private int counter=0;
 
 	public MoveEvery7Days(int x, int y, Country country) {
@@ -16,7 +16,7 @@ public class MoveEvery7Days extends Skeptic {
 	}
 
 	/**
-	 * add a restriction to the tryToMove method so that the person moves every 7 days. 
+	 * add a restriction to the tryToMove method so that the person moves every 7 days.
 	 */
 	public void tryToMove() {
 		if(this.counter%7==0) {
